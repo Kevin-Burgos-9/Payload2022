3b7593bff2bac3d6b3b820f87f6039d29f893585
yanitiger <yaniel.varela@upr.edu>
2023-01-28 12:17:34 -0400

    test no se

--- rover/cam.py
+++ rover/cam.py
@@ -44,13 +44,22 @@ class Camera:
         dt = datetime.now()
         print(str(dt))
 
+        img_original = cv2.imread('imagen.jpg')
+
         #cuando el valor adentro del parentesis esta es 1 producira la foto normal
         #cuando el valor de adentro es 0 producira la foto en grayscale
-        imgs = cv2.imread(img, 1)
+        imgs = cv2.imread(img, 0)
+
 
         #cuando el flip code es 0 la imagen rotara verticalmente
         #cuando el flip code es un numero positivo la imagen rotara horizontalmente
-        imgs = cv2.flip(imgs, -1)
+
+        imgs = cv2.flip(imgs,-1)
+
+        #esta bloque se utiliza para añadirle un filtro a la foto
+        #en este caso se le añadio un blur filter
+        ksize = (10,10)
+        imgs = cv2.blur(imgs, ksize)
 
 
         #Que hace cada funcion
@@ -59,7 +68,7 @@ class Camera:
         #frontScale - Tamaño de timestamp
         cv2.putText(img=imgs, text=str(dt), org =(250, 300),fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 0.75, color = (10, 235, 245))
 
-
+        cv2.imshow("Normal Image", img_original)
         cv2.imshow("image", imgs)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
