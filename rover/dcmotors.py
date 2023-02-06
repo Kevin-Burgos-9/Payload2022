5c067a0b53a43570ad9f9fb3369fe28ba449941d
sotam1069 <pedro.matos9@upr.edu>
2023-02-06 19:19:42 -0400

    added gpiozero and useful functions

--- rover/dcmotors.py
+++ rover/dcmotors.py
@@ -1,6 +1,23 @@
+from gpiozero import Motor
 class Dcmotors:
-    def __init__(self):
-        pass
+    def __init__(self, motor_pin: int):
+        self.motor_pin = motor_pin
+        self.motor = Motor(motor_pin)
+
+    def isOn(self) -> bool:
+        """ Detects if the motor is turned on """
+
+        return self.motor.is_active
+
+    def getPin(self) -> int:
+
+        return self.motor_pin
+
+    def isStuck(self) -> bool:
+
+        
+
+
 
     def move_rover(self, backwards: bool) -> None:
         """Function to move rover forward,
