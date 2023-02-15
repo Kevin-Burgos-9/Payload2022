01ea854e5006d491e44939fd27881ee0b731164a
JorgePrez465 <jorgelui.ph@gmail.com>
2023-02-15 11:59:54 -0400

    Modded main rover

--- rover/main_rover.py
+++ rover/main_rover.py
@@ -4,7 +4,7 @@ import keyboard
 while True:
     user_input = input("Please enter a command: ")
 
-    while not keyboard.is_pressed():
+    while not keyboard.is_pressed('space'):
         if user_input == "1":
             buzzer.buzzer_on()
         elif user_input == "2":
