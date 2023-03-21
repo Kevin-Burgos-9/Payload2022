9fc56a9a47f2e8a8544d5f42fb7a7989e574129a
Kevin Burgos <36522318+Kevin-Burgos-9@users.noreply.github.com>
2023-03-21 00:34:50 -0400

    Added simple treshold

--- rover/mpu.py
+++ rover/mpu.py
@@ -18,46 +18,63 @@ def checkTilt(aX, aY, aZ):
     else:
         return "Robot is not upside down"
 
+mpu.set_accel_range(mpu6050_sensor.ACCEL_RANGE_16G)
+
+okidokiToRecordy = False    
+
 while True:
 
-    accelerometer_data = mpu.get_accel_data()
+    LANDED = False
+    temperature = 0
+
+    accelerometer_data = mpu.get_accel_data(g=True)
     gyro_data = mpu.get_gyro_data()
 
-    prevX = round(accelerometer_data.get('x'),0)
-    prevY = round(accelerometer_data.get('y'),0)
-    prevZ = round(accelerometer_data.get('z'),0)
+    if accelerometer_data.get('x') > 3.0:
+        okidokiToRecordy = True
 
-    time.sleep(0.5)
+    if okidokiToRecordy:
+        prevX = round(accelerometer_data.get('x'),0)
+        prevY = round(accelerometer_data.get('y'),0)
+        prevZ = round(accelerometer_data.get('z'),0)
 
-    accelerometer_data = mpu.get_accel_data()
-    ax = round(accelerometer_data.get('x'),0)
-    ay = round(accelerometer_data.get('y'),0)
-    az = round(accelerometer_data.get('z'),0)
+        time.sleep(0.15)
 
-    print('X: ' + str(prevX) + ' Y: ' + str(prevY) + ' Z: '+ str(prevZ))
-    print('X: ' + str(ax) + ' Y: ' + str(ay) + ' Z: '+ str(az))
+        accelerometer_data = mpu.get_accel_data()
+        ax = round(accelerometer_data.get('x'),0)
+        ay = round(accelerometer_data.get('y'),0)
+        az = round(accelerometer_data.get('z'),0)
+
+        print('X: ' + str(prevX) + ' Y: ' + str(prevY) + ' Z: '+ str(prevZ))
+        print('X: ' + str(ax) + ' Y: ' + str(ay) + ' Z: '+ str(az))
 
 
-    #LANDED CODE
-    if (ax == prevX) and (ay == prevY) and (az == prevZ):
-        print('Landed!!!')
+        #LANDED CODE
+        if (ax == prevX) and (ay == prevY) and (az == prevZ):
+            print('Landed!!!')
+            LANDED = True
+            print(checkTilt(ax, ay, az))
+        
+        #NOT LANDED, STILL MOVING
+        else:
+            print('Schmooving!')
+            LANDED = False
+
+        temp = []
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        
+        temperature = mpu.get_temp()
+
+        temp.append(current_time)
+        temp.append(ax)
+        temp.append(ay)
+        temp.append(az)
+        temp.append(LANDED)
+        temp.append(temperature)
+
         print(checkTilt(ax, ay, az))
-    
-    #NOT LANDED, STILL MOVING
-    else:
-        print('Schmooving!')
 
-    temp = []
-    t = time.localtime()
-    current_time = time.strftime("%H:%M:%S", t)
+        record.record(temp)
 
-    temp.append(current_time)
-    temp.append(ax)
-    temp.append(ay)
-    temp.append(az)
-
-    print(checkTilt(ax, ay, az))
-
-    record.record(temp)
-
-    time.sleep(0.5)
+        time.sleep(0.5)
