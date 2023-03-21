416c42679556f3baffb23acad16a10561ba746ee
JorgePrez465 <jorgelui.ph@gmail.com>
2023-03-21 00:18:31 -0400

    again

--- rover/test_mpu.py
+++ rover/test_mpu.py
@@ -1,13 +1,15 @@
 from mpu6050 import mpu6050
+import time
 
 mpu6050_sensor = mpu6050(0x68)  # MPU6050 device address
 
 while True:
     # Read accelerometer data
     mpu6050_sensor.set_accel_range(mpu6050_sensor.ACCEL_RANGE_16G)
-    accel_data = mpu6050_sensor.get_accel_data()
+    accel_data = mpu6050_sensor.get_accel_data(g=True)
 
     # Print acceleration data
     print("Acceleration in X-Axis : %f g" % accel_data['x'])
     print("Acceleration in Y-Axis : %f g" % accel_data['y'])
     print("Acceleration in Z-Axis : %f g" % accel_data['z'])
+    time.sleep(0.5)
