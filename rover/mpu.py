17ce02146a54fcdf702f11a2a9fe8d443cd55c83
JorgePrez465 <jorgelui.ph@gmail.com>
2023-03-27 23:26:47 -0400

    more testing

--- rover/mpu.py
+++ rover/mpu.py
@@ -33,9 +33,7 @@ while True:
 
     accelerometer_data = mpu.get_accel_data(g=True)
     gyro_data = mpu.get_gyro_data()
-    print("> 5", round(accelerometer_data.get('x'), 0))
-    print(" < -5", round(accelerometer_data.get('x'), 0))
-    time.sleep(1)
+
     if round(accelerometer_data.get('x'), 0) > 2 or round(accelerometer_data.get('x'), 0) < -2:
         okidokiToCheckyForLandy = True
         print("Launched")
